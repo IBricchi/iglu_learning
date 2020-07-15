bb14f849568f28803d4eb0e91dc3c0acc1fce903
IBricchi <ignacio.bricchi@gmail.com>
2020-07-15 13:08:21 +0200

    Setup basic tool project

---
+++ Tool/GenerateAst.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tool
+{
+	static class GenerateAst
+	{
+		public static void SubMain(List<string> args)
+		{
+			
+		}
+	}
+}
---
+++ Tool/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tool
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			if(args.Length == 0)
+			{
+				Console.Error.WriteLine("Usage: Tool <tool name> <Tool Parameters>");
+			}
+			else
+			{
+				List<string> newArgs = args.ToList<string>();
+				newArgs.RemoveAt(0);
+				switch (args[0])
+				{
+					case "GenerateAst":
+						GenerateAst.SubMain(newArgs);
+						break;
+					default:
+						Console.Error.WriteLine("Error: Unknown tool name)");
+						break;
+				}
+
+			}
+		}
+	}
+}
