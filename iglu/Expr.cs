a2af5c37a9967c5c326b2358cc785f7387770ff9
IBricchi <ignacio.bricchi@gmail.com>
2020-07-15 12:38:19 +0200

    Add expresion class with basic binary expression

---
+++ iglu/Expr.cs
@@ -0,0 +1,30 @@
+﻿using Iglu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iglu
+{
+	abstract class Expr
+	{
+		class Binary : Expr
+		{
+			public Binary(Expr left, Token oper, Expr right)
+			: base(left, oper, right) { }
+		}
+
+		public Expr(Expr left, Token oper, Expr right)
+		{
+			this.left = left;
+			this.oper = oper;
+			this.right = right;
+		}
+
+		internal readonly Expr left;
+		internal readonly Token oper;
+		internal readonly Expr right;
+
+	}
+}
