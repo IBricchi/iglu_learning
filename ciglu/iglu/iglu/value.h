99f62ce6447d57071ebd3ef03d8f1ed2169b1b48
IBricchi <ignacio.bricchi@gmail.com>
2020-07-25 23:00:52 +0300

    Add binary expressions and operators

--- ciglu/iglu/iglu/value.h
+++ ciglu/iglu/iglu/value.h
@@ -2,8 +2,30 @@
 
 #include "common.h"
 
-typedef double Value;
-void printValue(Value value);
+typedef enum {
+	VAL_BOOL,
+	VAL_NIL,
+	VAL_NUMBER,
+} ValueType;
+
+typedef struct {
+	ValueType type;
+	union {
+		bool boolean;
+		double number;
+	} as;
+} Value;
+
+#define IS_BOOL(value)    ((value).type == VAL_BOOL)
+#define IS_NIL(value)     ((value).type == VAL_NIL)
+#define IS_NUMBER(value)  ((value).type == VAL_NUMBER)
+
+#define AS_BOOL(value)    ((value).as.boolean)
+#define AS_NUMBER(value)  ((value).as.number)
+
+#define BOOL_VAL(value)   ((Value){ VAL_BOOL, { .boolean = value } })
+#define NIL_VAL           ((Value){ VAL_NIL, { .number = 0 } })
+#define NUMBER_VAL(value) ((Value){ VAL_NUMBER, { .number = value } })
 
 typedef struct {
 	int count;
@@ -11,6 +33,9 @@ typedef struct {
 	Value* values;
 } ValueArray;
 
+void printValue(Value value);
+
+bool valuesEqual(Value a, Value b);
 void initValueArray(ValueArray* array);
 void freeValueArray(ValueArray* array);
 void writeValueArray(ValueArray* array, Value value);
